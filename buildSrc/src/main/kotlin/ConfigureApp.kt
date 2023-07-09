c7d3e586317710852fb11de36251785029400dae
afoxpluss <92241438+afoxpluss@users.noreply.github.com>
2023-07-08 21:11:23 -0500

    Initial commit

---
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -0,0 +1,13 @@
+object ConfigureApp {
+    //TODO change values with your info [APP]
+    const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/your_path_module"
+
+    //TODO change values with your info [Github Artifact]
+    const val groupId = "com.afoxplus.android"
+    const val artifactId = "module"
+    const val version = "1.0.0"
+
+    //TODO change values with your info [SonarQube]
+    const val organization = "afoxplus"
+    const val projectName = "app-android-${artifactId}"
+}
---
+++ module/build.gradle.kts
@@ -0,0 +1,115 @@
+plugins {
+    id("com.android.library") version "7.3.1"
+    id("org.jetbrains.kotlin.android") version "1.7.20"
+    id("org.jetbrains.kotlin.kapt") version "1.7.20"
+    id("com.google.dagger.hilt.android") version "2.44.2"
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.7.20"
+    id("org.sonarqube") version "3.3"
+    id("jacoco")
+}
+
+apply {
+    from("sonarqube.gradle")
+    from("jacoco.gradle")
+    from("upload.gradle")
+}
+
+android {
+    namespace = "com.afoxplus.name_module"
+    compileSdk = Versions.compileSdkVersion
+
+    defaultConfig {
+        minSdk = Versions.minSdkVersion
+        targetSdk = Versions.targetSdkVersion
+        testInstrumentationRunner = Versions.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+        renderscriptSupportModeEnabled = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        create("staging") {
+            initWith(getByName("debug"))
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("debug") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    lint {
+        disable.addAll(
+            listOf(
+                "TypographyFractions",
+                "TypographyQuotes",
+                "JvmStaticProvidesInObjectDetector",
+                "FieldSiteTargetOnQualifierAnnotation",
+                "ModuleCompanionObjects",
+                "ModuleCompanionObjectsNotInModuleParent"
+            )
+        )
+        checkDependencies = true
+        abortOnError = false
+        ignoreWarnings = false
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree("libs") { include(listOf("*.jar", "*.aar")) })
+    implementation(Deps.Jetpack.kotlin)
+    implementation(Deps.Jetpack.core)
+    implementation(Deps.Jetpack.appcompat)
+    implementation(Deps.Jetpack.activity)
+    implementation(Deps.Jetpack.fragment)
+    implementation(Deps.UI.materialDesign)
+    implementation(Deps.UI.constraintLayout)
+    implementation(Deps.Arch.coroutinesCore)
+    implementation(Deps.Arch.hiltAndroid)
+    kapt(Deps.Arch.hiltCompiler)
+
+    //Business Dependencies
+    implementation(Deps.UI.uikit)
+    implementation(Deps.Arch.network)
+
+    testImplementation(Deps.Test.jUnit)
+    testImplementation(Deps.Test.testCore)
+    testImplementation(Deps.Test.truth)
+    testImplementation(Deps.Test.mockitoKotlin)
+    androidTestImplementation(Deps.Test.androidJUnit)
+    androidTestImplementation(Deps.Test.espresso)
+}
