9751b66a7de46bc960dc2746d22266a797bbe2a9
Valentin Mendoza Torres <jmendozat13@gmail.com>
2023-07-09 17:17:05 -0500

    Initial commit

---
+++ demo/build.gradle.kts
@@ -0,0 +1,111 @@
+plugins {
+    id("com.android.application") version "7.3.1"
+    id("org.jetbrains.kotlin.android") version "1.7.20"
+    id("org.jetbrains.kotlin.kapt") version "1.7.20"
+    id("com.google.dagger.hilt.android") version "2.44.2"
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.7.20"
+    id("org.sonarqube") version "3.3"
+    id("jacoco")
+}
+
+android {
+    namespace = "com.afoxplus.module.demo"
+    compileSdk = Versions.compileSdkVersion
+
+    defaultConfig {
+        applicationId = "${ConfigureApp.groupId}.${ConfigureApp.artifactId}"
+        minSdk = Versions.minSdkVersion
+        targetSdk = Versions.targetSdkVersion
+        versionCode = 1
+        versionName = "1.0.0"
+        testInstrumentationRunner = Versions.testInstrumentationRunner
+    }
+
+    buildTypes {
+        getByName("release") {
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+
+        create("staging") {
+            initWith(getByName("debug"))
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            applicationIdSuffix = ".staging"
+            versionNameSuffix = "-staging"
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("debug") {
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    lint {
+        disable.addAll(
+            listOf(
+                "TypographyFractions",
+                "TypographyQuotes",
+                "JvmStaticProvidesInObjectDetector",
+                "FieldSiteTargetOnQualifierAnnotation",
+                "ModuleCompanionObjects",
+                "ModuleCompanionObjectsNotInModuleParent"
+            )
+        )
+        checkDependencies = true
+        abortOnError = false
+        ignoreWarnings = false
+    }
+}
+
+dependencies {
+    implementation(fileTree("libs") { include(listOf("*.jar", "*.aar")) })
+    implementation(Deps.Jetpack.kotlin)
+    implementation(Deps.Jetpack.core)
+    implementation(Deps.Jetpack.appcompat)
+
+    implementation(Deps.Jetpack.activity)
+    implementation(Deps.Jetpack.fragment)
+
+    implementation(Deps.UI.materialDesign)
+    implementation(Deps.UI.constraintLayout)
+
+    implementation(Deps.Arch.coroutinesCore)
+    implementation(Deps.Arch.hiltAndroid)
+    kapt(Deps.Arch.hiltCompiler)
+
+    testImplementation(Deps.Test.jUnit)
+    testImplementation(Deps.Test.testCore)
+    testImplementation(Deps.Test.truth)
+    testImplementation(Deps.Test.mockitoKotlin)
+    androidTestImplementation(Deps.Test.androidJUnit)
+    androidTestImplementation(Deps.Test.espresso)
+}
